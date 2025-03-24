fc22e74c6b4e647df39a20a46074c180c4c7004d
Sagarmundinamani <mundinamanisagar@gmail.com>
2025-03-24 16:09:23 +0530

    updated

--- src/Arrays/New1.java
+++ src/Arrays/New1.java
@@ -1,7 +1,28 @@
 package Arrays;
 
+import java.util.Scanner;
+
 public class New1 {
     public static void main(String[] args) {
-
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter the number");
+        int n=sc.nextInt();
+        if(isprime(n)==1){
+            System.out.println("prime");
+        }else{
+            System.out.println("not prime");
+        }
+    }
+    public static int isprime(int n){
+        int count=0;
+        if(n<=1){
+            count=0;
+        }
+     for(int i=1; i<=Math.sqrt(n); i++){
+         if(n%i==0){
+             count++;
+         }
+     }
+     return count;
     }
 }
---
+++ src/Arrays/New2.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class New2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        int n=sc.nextInt();
+        System.out.println(expo(num,n));
+    }
+    public static int expo(int num,int n ){
+        int sum=1;
+        for(int i=1; i<=n; i++){
+           sum*=num;
+        }
+        return sum;
+    }
+}
