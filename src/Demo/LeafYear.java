6a5d9b45e677f491932379a71a7155eb9beb4881
Sagarmundinamani <mundinamanisagar@gmail.com>
2025-04-01 15:02:31 +0530

    updated

--- src/Demo/LeafYear.java
+++ src/Demo/LeafYear.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
     public class LeafYear {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-            System.out.println("enter the year");
+            System.out.println("enter the year:");
             int year = sc.nextInt();
             if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
                 System.out.println(year + " leaf year");
---
+++ src/Exception/New.java
@@ -0,0 +1,12 @@
+package Exception;
+
+public class New {
+    public static void main(String[] args) {
+
+//            float a=10,b=0;
+//            float res=a/b;
+//        System.out.println(res);//ArithmeticException
+             String s1=null;
+        System.out.println(s1.length());//NullPointerException
+    }
+}
