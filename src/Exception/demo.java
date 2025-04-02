33e9c7cd2e93cbee38ed7910601af64333a296c2
Sagarmundinamani <mundinamanisagar@gmail.com>
2025-04-02 23:53:54 +0530

    updated

--- src/Exception/demo.java
+++ src/Exception/demo.java
@@ -1,4 +1,13 @@
 package Exception;
 
 public class demo {
+    public static void main(String[] args) {
+        int a=12, b=11;
+        ArithmeticException a1=new ArithmeticException();
+        if(a>b){
+            throw a1;
+        }else{
+            throw new NullPointerException();
+        }
+    }
 }
