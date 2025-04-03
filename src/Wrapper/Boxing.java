dc6d707ee387843240b9b0f95d665296bd5b3dc9
Sagarmundinamani <mundinamanisagar@gmail.com>
2025-04-03 10:09:40 +0530

    updated

---
+++ src/Wrapper/Boxing.java
@@ -0,0 +1,9 @@
+package Wrapper;
+
+public class Boxing {
+    public static void main(String[] args) {
+        int a=25;
+        Integer i=Integer.valueOf(a);//boxing
+        System.out.println(i);
+    }
+}
