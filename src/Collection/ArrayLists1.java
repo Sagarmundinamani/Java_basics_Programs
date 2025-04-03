70e4fee289fdd22fd46b0f4c78523783fcd3ca36
Sagarmundinamani <mundinamanisagar@gmail.com>
2025-04-03 15:51:32 +0530

    updated

--- src/Collection/ArrayLists1.java
+++ src/Collection/ArrayLists1.java
@@ -1,22 +1,35 @@
 package Collection;
 
+import com.sun.security.jgss.GSSUtil;
+
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class ArrayLists1 {
     public static void main(String[] args) {
-        ArrayList a1=new ArrayList<>();
+        ArrayList<Integer> a1=new ArrayList<>();
         a1.add(14);
         a1.add(18);
-        a1.add("sagar");
-        a1.add(false);
-        a1.add('x');
-        a1.add(13.18);
-        a1.add(18);
+        a1.add(20);
+//        a1.add(false);
+//        a1.add('x');
+//        a1.add(13.18);
+//        a1.add(18);
 //        for(int i=0; i<a1.size(); i++){
 //            System.out.println(a1.get(i));
-        for(Object o:a1){
-            System.out.println(o);
+//        for(Object o:a1){
+//            System.out.println(o);
+//        }
+        Iterator i= a1.iterator();
+//        System.out.println(i.hasNext());
+//        System.out.println(i.next());
+//        System.out.println(i.hasNext());
+//        System.out.println(i.next());
+//        System.out.println(i.hasNext());
+//        System.out.println(i.next());
+//        System.out.println(i.hasNext());
+        while(i.hasNext()){
+            System.out.println(i.next());
         }
-
     }
 }
