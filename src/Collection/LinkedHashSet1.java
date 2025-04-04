d46f8a40478bc5dea1a43aa0fb7a1df35bd8d3cf
Sagarmundinamani <mundinamanisagar@gmail.com>
2025-04-04 12:50:46 +0530

    updated

---
+++ src/Collection/LinkedHashSet1.java
@@ -0,0 +1,14 @@
+package Collection;
+
+import java.util.LinkedHashSet;
+
+public class LinkedHashSet1 {
+    public static void main(String[] args) {
+        LinkedHashSet l=new LinkedHashSet();
+        l.add(10);
+        l.add(20);
+        l.add(10);
+        l.add("sagar");
+        System.out.println(l);
+    }
+}
