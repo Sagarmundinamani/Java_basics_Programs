b52233664725531730a1e183946e00b1bd6d187c
Sagarmundinamani <mundinamanisagar@gmail.com>
2025-04-05 12:54:38 +0530

    updated

---
+++ src/Collection/HashTabledemo.java
@@ -0,0 +1,13 @@
+package Collection;
+
+import java.util.Hashtable;
+
+public class HashTabledemo {
+    public static void main(String[] args) {
+        Hashtable ht=new Hashtable();
+        ht.put(17,"hi");
+        ht.put(23.80,"uo");
+        ht.put(false,true);
+        System.out.println(ht);
+    }
+}
